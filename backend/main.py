855f715233af65959f9424840efe2d0bcd75dd65
Kajal-Gupta23 <kajal.gupta@probeinformation.com>
2025-07-16 02:51:03 +0530

    added cors

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,19 @@ from slowapi.errors import RateLimitExceeded
 from slowapi import _rate_limit_exceeded_handler
 from routers.analysis import router as analysis_router
 from routers.conversation import router as conversation_router
+from fastapi.middleware.cors import CORSMiddleware
 from dependencies import limiter
 
 
 app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],  
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 
 app.include_router(analysis_router)
 app.include_router(conversation_router)
