bc7b4aeafa85e553bfea25c2aa73669692a57d2a
Kajal-Gupta23 <kajal.gupta@probeinformation.com>
2025-07-16 02:14:14 +0530

    changed configurations

--- backend/config.py
+++ backend/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = ""
-    GEMINI_API_KEY = ""
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
     RATE_LIMIT = "5/minute"
     CONVERSE_RATE_LIMIT = "10/minute"
 
